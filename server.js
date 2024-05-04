58e92c12362e74f1a7b17818c31e560d59851f1a
RvMern <rkv51508@gmail.com>
2024-05-04 18:49:56 +0530

    First Commit

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require("./config/dbConnect");
 // ! Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join('C:\Download\PROJECTS\Expenses Tracker App','/Expenses Tracker Frontend/build')));
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
@@ -19,6 +20,11 @@ app.use(cors());
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/account",accountRouter);
 app.use("/api/v1/transaction",transactionRouter);
+
+// Catch-all route to serve React's index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join('C:\Download\PROJECTS\Expenses Tracker App','/Expenses Tracker Frontend/build/index.html'));
+  });
 
 // TODO : Error Handler
 app.use(errorHandler);
