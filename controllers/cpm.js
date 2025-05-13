bd07b9ac65bd6d46b372d1b18c82a962ea6e3840
Rupesh Kumar Verma <149824457+RvMern@users.noreply.github.com>
2025-05-13 10:23:17 +0530

    Update cpm.js

--- controllers/cpm.js
+++ controllers/cpm.js
@@ -1,7 +1,7 @@
 const Chiller = require("../models/ChillerModel.js");
 const mongoose = require("mongoose");
 
-const getChiller = async(req,res,next) => {
+const getChillerData = async(req,res,next) => {
     try{
         const chillerID = req.params.id;
         const chillerData = await Chiller.find({ id: chillerID });
@@ -15,8 +15,30 @@ const getChiller = async(req,res,next) => {
     }
 }
 
-const createAccount = async(req,res,next) => {
-}
-
-const updateAccount = async(req,res,next) => {
+const createChillerData = async(req,res,next) => {
+    try{
+        const {id, email, password} = req.body;
+        if(fullname === "" || email === "" || password === ""){
+            return next(passError(400,"All fields are required"));
+        }
+        const profilePhoto = req.file ? req.file.path : undefined;
+        const foundUser = await User.findOne({email});
+        if(!foundUser){
+            const newUser = await User.create({
+                fullname,
+                email,
+                profileImage: profilePhoto,
+                password
+            });
+            return res.status(200).json({
+                success:true,
+                message: "New User Has Been Created Successfully",
+                newUser
+            });
+        }
+        next(passError(400,"User Already Exists"));
+    }
+    catch(err){
+        next(passError(500,err.message));
+    }
 }
