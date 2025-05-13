26ddef3db44d301f9a67b648950957ff6e97976e
Rupesh Kumar Verma <149824457+RvMern@users.noreply.github.com>
2025-05-13 10:12:17 +0530

    Update cpmRoute.js

--- routes/cpmRoute.js
+++ routes/cpmRoute.js
@@ -1,1 +1,11 @@
+const express = require("express");
+// const { registerUser, loginUser, getUserProfile, updateUserProfileDetails, updateUserPassword, deleteUser, logoutUser } = require("../controllers/userCtrl");
+// const isLoggedIn = require("../middlewares/isLoggedIn");
+const route = express.Router();
 
+route.post('/register', upload.single("file"), registerUser);
+route.put('/register', upload.single("file"), registerUser);
+route.get('/userprofile', isLoggedIn, getUserProfile);
+
+
+module.exports = route
