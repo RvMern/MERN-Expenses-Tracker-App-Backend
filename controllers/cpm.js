f228d889ee7386870d7196b790e2969782d72664
Rupesh Kumar Verma <149824457+RvMern@users.noreply.github.com>
2025-05-13 10:16:12 +0530

    Update cpm.js

--- controllers/cpm.js
+++ controllers/cpm.js
@@ -1,4 +1,20 @@
-const getSingleAccount = async(req,res,next) => {
+
+
+const getChiller = async(req,res,next) => {
+    try{
+        const acconntID = req.params.id;
+        const findAccount = await Account.findById(acconntID).populate('transactions');
+        if(!findAccount){
+            return next(passError(404,"Unable to find! Invalid Account ID"));
+        }
+        res.status(200).json({
+            success:true,
+            Account:findAccount
+        });
+    }
+    catch(err){
+        next(passError(500,err.message));
+    }
 }
 
 const createAccount = async(req,res,next) => {
