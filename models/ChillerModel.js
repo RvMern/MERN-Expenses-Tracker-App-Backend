0a2209cc4031a5c2d919a8b7ad4fffe60046787a
Rupesh Kumar Verma <149824457+RvMern@users.noreply.github.com>
2025-05-13 10:10:01 +0530

    Update ChillerModel.js

--- models/ChillerModel.js
+++ models/ChillerModel.js
@@ -1,1 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const chillerSchema = new mongoose.Schema({
+  dataname: {
+    type: String,
+    required: true
+  },
+  setpoint: {
+    type: Number,
+    required: true
+  },
+  loadpercent: {
+    type: Number,
+    required: true
+  },
+  rangeMin: {
+    type: Number, // e.g., 1
+    required: true
+  },
+  rangeMax: {
+    type: Number, // e.g., 5
+    required: true
+  },
+  status: {
+    type: Boolean,
+    required: true
+  }
+},{
+    timestamps:true
+});
+
+module.exports = new mongoose.model("Chiller",chillerSchema);
+
+
+
 
