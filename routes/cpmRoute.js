5da8a03c569ba4dca920ea558863fb0f5901c323
Rupesh Kumar Verma <149824457+RvMern@users.noreply.github.com>
2025-05-14 11:17:09 +0530

    Update cpmRoute.js

--- routes/cpmRoute.js
+++ routes/cpmRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createChillerData, getChillerData, updateChillerData } = require("../controllers/cpm.js");
+const { createChillerData, getChillerData, updateChillerData, getAllChillersData } = require("../controllers/cpm.js");
 const route = express.Router();
 
 route.post('/createchillerdata',createChillerData);
 route.put('/updatechillerdata/:id', updateChillerData);
 route.get('/getchillerdata/:id', getChillerData );
+route.get('/getallchillersdata', getAllChillersData );
 
 module.exports = route
